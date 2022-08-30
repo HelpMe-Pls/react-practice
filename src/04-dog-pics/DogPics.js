40c34d6f2e9817a0e2935e63c94c9069f3b9d78e
Lê Tấn Khôi <43715386+HelpMe-Pls@users.noreply.github.com>
2022-08-30 17:30:01 +0700

    04

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,21 @@
-export default function DogPics () {
-  // API: https://dog.ceo/dog-api/
-  return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
-    </div>
-  )
+import { useState, useEffect } from 'react'
+
+export default function DogPics() {
+	// API: https://dog.ceo/dog-api/
+	const [dog, setDog] = useState('')
+
+	async function getDog() {
+		const response = await fetch('https://dog.ceo/api/breeds/image/random')
+		const result = await response.json()
+		setDog(result.message)
+	}
+
+	useEffect(() => getDog(), [])
+
+	return (
+		<div className="dog-pics">
+			<img src={dog} alt="some random dog" />
+			<button onClick={getDog}>🐶</button>
+		</div>
+	)
 }
