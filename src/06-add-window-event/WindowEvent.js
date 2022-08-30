b349b4195815ee14be2a9589b99aed39b59ed175
Lê Tấn Khôi <43715386+HelpMe-Pls@users.noreply.github.com>
2022-08-30 18:19:30 +0700

    6

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,5 +1,16 @@
-export default function WindowEvent () {
-  return (
-    <h2>Window event active</h2>
-  )
+import { useEffect } from 'react'
+
+export default function WindowEvent() {
+	function Yell() {
+		alert('You fuckin moron!')
+	}
+
+	useEffect(() => {
+		window.addEventListener('dblclick', Yell)
+
+		// when the `windowEvent` state changes, this component rerenders, and this function kicks in BEFORE that rerender.
+		return () => window.removeEventListener('dblclick', Yell)
+	}, [])
+
+	return <h2>Window event is active</h2>
 }
