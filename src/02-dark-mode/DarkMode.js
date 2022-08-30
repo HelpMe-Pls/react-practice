c0f97fe814c29cbe12b69eb031e0b4e95ed1f67a
Lê Tấn Khôi <43715386+HelpMe-Pls@users.noreply.github.com>
2022-08-30 12:11:59 +0700

    02

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,22 @@
-export default function DarkMode () {
-  return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
-    </div>
-  )
+import { useState } from 'react'
+
+export default function DarkMode() {
+	const [theme, setTheme] = useState('page')
+
+	return (
+		<div className={theme}>
+			<button
+				className="dark-mode-button"
+				onClick={(_e) => setTheme('page dark-mode')}
+			>
+				Dark Mode
+			</button>
+			<button
+				className="light-mode-button"
+				onClick={(_e) => setTheme('page')}
+			>
+				Light Mode
+			</button>
+		</div>
+	)
 }
