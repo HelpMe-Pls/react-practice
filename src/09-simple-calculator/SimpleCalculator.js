5640a0ab6a92e34f45ca0d64d5ff9fe91cf595a8
Lê Tấn Khôi <43715386+HelpMe-Pls@users.noreply.github.com>
2022-08-31 15:58:45 +0700

    9

--- src/09-simple-calculator/SimpleCalculator.js
+++ src/09-simple-calculator/SimpleCalculator.js
@@ -1,33 +1,73 @@
-const initialState = {}
+import { useReducer } from 'react'
 
-function reducer (state, action) {}
+function reducer(state, action) {
+	switch (action.type) {
+		case 'NUM_1':
+			return { ...state, op1: action.number }
+		case 'NUM_2':
+			return { ...state, op2: action.number }
+		case 'ADD':
+			return { ...state, res: state.op1 + state.op2 }
+		case 'SUBTRACT':
+			return { ...state, res: state.op1 - state.op2 }
+		default:
+			return initialState
+	}
+}
 
-export default function SimpleCalculator () {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const initialState = {
+	op1: 0,
+	op2: 0,
+	res: 0,
+}
 
-  return (
-    <div>
-      <div>
-        <h2>Number 1</h2>
-        {numbers.map(number => (
-          <button key={number}>
-            {number}
-          </button>))}
-      </div>
-      <div>
-        <h2>Number 2</h2>
-        {numbers.map(number => (
-          <button key={number}>
-            {number}
-          </button>))}
-      </div>
-      <div>
-        <h2>Actions</h2>
-        <button>+</button>
-        <button>-</button>
-        <button>c</button>
-      </div>
-      <div><h2>Result:</h2></div>
-    </div>
-  )
+export default function SimpleCalculator() {
+	const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+	const [result, dispatchAction] = useReducer(reducer, initialState)
+
+	return (
+		<div>
+			<div>
+				<h2>Number 1</h2>
+				{numbers.map((number) => (
+					<button
+						key={number}
+						onClick={() =>
+							dispatchAction({ type: 'NUM_1', number })
+						}
+					>
+						{number}
+					</button>
+				))}
+			</div>
+			<div>
+				<h2>Number 2</h2>
+				{numbers.map((number) => (
+					<button
+						key={number}
+						onClick={() =>
+							dispatchAction({ type: 'NUM_2', number })
+						}
+					>
+						{number}
+					</button>
+				))}
+			</div>
+			<div>
+				<h2>Actions</h2>
+				<button onClick={() => dispatchAction({ type: 'ADD' })}>
+					+
+				</button>
+				<button onClick={() => dispatchAction({ type: 'SUBTRACT' })}>
+					-
+				</button>
+				<button onClick={() => dispatchAction({ type: 'whatever' })}>
+					c
+				</button>
+			</div>
+			<div>
+				<h2>Result: {result.res}</h2>
+			</div>
+		</div>
+	)
 }
