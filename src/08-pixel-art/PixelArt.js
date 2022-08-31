9ddf602840aef4b221945f15e3381db5eca10191
Lê Tấn Khôi <43715386+HelpMe-Pls@users.noreply.github.com>
2022-08-31 12:32:32 +0700

    8

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,34 +1,101 @@
-import React from 'react'
+import React, { useState, createContext, useContext } from 'react'
 
-function ColorPicker () {
-  const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
-  return (
-    <div>
-      <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} />)}
-    </div>
-  )
+const ColorContext = createContext({
+	color: 'lightGrey',
+	setColor: () => {
+		/*placeHolder */
+	},
+})
+ColorContext.displayName = 'ColorContext'
+
+function ColorProvider({ children }) {
+	const [color, setColor] = useState('lightGrey')
+	return (
+		<ColorContext.Provider value={[color, setColor]}>
+			{children}
+		</ColorContext.Provider>
+	)
 }
 
-function Pixel () {
-  return <div style={{ height: '20px', width: '20px', backgroundColor: 'lightGrey', margin: '1px' }} />
+export function useColor() {
+	const context = useContext(ColorContext)
+	if (context === undefined) {
+		throw new Error(`useColor must be used within a ColorProvider`)
+	}
+	return context
 }
 
-function Pixels () {
-  const pixels = []
-  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />)
-  return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', width: '210px', margin: '0 auto' }}>
-      {pixels}
-    </div>
-  )
+function ColorPicker() {
+	const colors = [
+		'red',
+		'blue',
+		'yellow',
+		'green',
+		'black',
+		'cyan',
+		'purple',
+		'pink',
+	]
+	// Use array destructuring so it has to be in the same order as the original array
+	// If we're using object destructuring then it's just `{setColor}`
+	const [, setColor] = useColor()
+
+	return (
+		<>
+			<div style={{ marginLeft: 48 }}>
+				<h1>Choose a color</h1>
+				{colors.map((square) => (
+					<button
+						key={square}
+						style={{ backgroundColor: square }}
+						onClick={(_e) => setColor(square)}
+					/>
+				))}
+			</div>
+		</>
+	)
 }
 
-export default function PixelArt () {
-  return (
-    <div>
-      <ColorPicker />
-      <Pixels />
-    </div>
-  )
+function Pixel() {
+	const [backgroundColor, setBackgroundColor] = useState('lightGrey')
+	const [color] = useColor()
+
+	return (
+		<button
+			style={{
+				height: '20px',
+				width: '20px',
+				backgroundColor: backgroundColor,
+				margin: '1px',
+			}}
+			onClick={(_e) => setBackgroundColor(color)}
+		/>
+	)
+}
+
+function Grid() {
+	const pixels = []
+
+	for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />)
+	return (
+		<div
+			style={{
+				display: 'grid',
+				gridTemplateColumns: 'repeat(10, 1fr)',
+				width: '210px',
+				margin: '0 auto',
+			}}
+		>
+			{pixels}
+		</div>
+	)
+}
+
+export default function PixelArt() {
+	return (
+		<ColorProvider>
+			<ColorPicker />
+			<Grid />
+		</ColorProvider>
+	)
 }
